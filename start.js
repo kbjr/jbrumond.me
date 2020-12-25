25cae7401d4994b614732fad6daf55e711437449
James Brumond <james@jbrumond.me>
2020-12-24 16:18:18 -0800

    basic server in place to serve files

---
+++ start.js
@@ -0,0 +1,26 @@
+
+const { Server } = require('node-static');
+const { createServer } = require('@celeri/http-server');
+
+const server = createServer();
+const static_www = new Server(__dirname + '/www');
+
+const host = '0.0.0.0';
+const port = 8080;
+
+server.use(server.router({
+	notFound: function notFoundHandler({ req, res }) {
+		static_www.serve(req, res);
+	}
+}));
+
+server
+	.get('/test')
+	.use(({ req, res }) => {
+		res.writeHead(200, { 'content-type': 'text/plain' });
+		res.end('success');
+	});
+
+server.server.listen(port, host, () => {
+	console.log(`Listening for HTTP traffic at ${host}:${port}...`);
+});
